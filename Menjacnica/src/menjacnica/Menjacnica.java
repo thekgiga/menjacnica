bed57e8f63fbf5a3d04679473e176878d83a7e67
Thekgiga <thekgiga@gmail.com>
2015-03-29 00:57:00 +0100

    U klasi "Menjacnica", iz paketa "menjacnica", delimicno implementirane metode interfejsa "MenjacnicaInterface".

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,9 +1,27 @@
 package menjacnica;
 
+import interfejsMenjacnica.MenjacnicaInterface;
+
+import java.util.GregorianCalendar;
 import java.util.LinkedList;
 
-public class Menjacnica {
+public class Menjacnica implements MenjacnicaInterface {
 	
 	private LinkedList<Valuta> kursnaLista = new LinkedList<Valuta>();
+
+	public void dodajKurs(GregorianCalendar datum, String kupovniKurs,
+			String srednjiKurs, String prodajniKurs) {
+		
+	}
+
+	public void obrisiKurs(GregorianCalendar datum) {
+		
+		
+	}
+
+	public Valuta pronadjiKurs(GregorianCalendar datum) {
+		
+		return null;
+	}
 
 }
