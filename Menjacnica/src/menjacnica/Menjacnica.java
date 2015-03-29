53b896053a84f5ebab7d0e60e5900c21128c513a
Thekgiga <thekgiga@gmail.com>
2015-03-29 01:38:06 +0100

    Redefinisan interfejs "MenjacnicaInterface" i na master grani, i implemnetirana metoda za dodavanje kursa u klasi Menjacnica.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -9,19 +9,33 @@ public class Menjacnica implements MenjacnicaInterface {
 	
 	private LinkedList<Valuta> kursnaLista = new LinkedList<Valuta>();
 
-	public void dodajKurs(GregorianCalendar datum, String kupovniKurs,
-			String srednjiKurs, String prodajniKurs) {
+	public void dodajKurs(GregorianCalendar datum, Double kupovniKurs,
+			Double srednjiKurs, Double prodajniKurs, String naziv,
+			String skraceniNaziv) {
+	
+		Valuta v = new Valuta();
+		
+		v.setDatum(datum);
+		v.setKupovniKurs(kupovniKurs);
+		v.setProdajniKurs(prodajniKurs);
+		v.setSrednjiKurs(srednjiKurs);
+		v.setNaziv(naziv);
+		v.setSkracenNaziv(skraceniNaziv);
+		
+		kursnaLista.add(v);
+
+	}
+
+	public void obrisiKurs(GregorianCalendar datum, String naziv) {
+		// TODO Auto-generated method stub
 		
 	}
 
-	public void obrisiKurs(GregorianCalendar datum) {
-		
-		
-	}
-
-	public Valuta pronadjiKurs(GregorianCalendar datum) {
-		
+	public Valuta pronadjiKurs(GregorianCalendar datum, String naziv) {
+		// TODO Auto-generated method stub
 		return null;
 	}
+
+	
 
 }
