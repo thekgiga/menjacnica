de1ff8bd297327b3e6cb9629cae9f2e0df03a526
Thekgiga <thekgiga@gmail.com>
2015-03-29 01:31:26 +0100

    Redefinisane metode u interfejsu "menjacnicaInterface", a nakon toga i implementirane u klasi "Menjacnica".

--- Menjacnica/src/interfejsMenjacnica/MenjacnicaInterface.java
+++ Menjacnica/src/interfejsMenjacnica/MenjacnicaInterface.java
@@ -6,8 +6,9 @@ import menjacnica.Valuta;
 
 public interface MenjacnicaInterface {
 	
-	public void dodajKurs(GregorianCalendar datum, String kupovniKurs, String srednjiKurs, String prodajniKurs);
-	public void obrisiKurs(GregorianCalendar datum);
-	public Valuta pronadjiKurs (GregorianCalendar datum);
+	public void dodajKurs(GregorianCalendar datum, Double kupovniKurs, Double srednjiKurs, Double prodajniKurs,
+			String naziv, String skraceniNaziv);
+	public void obrisiKurs(GregorianCalendar datum, String naziv);
+	public Valuta pronadjiKurs (GregorianCalendar datum, String naziv);
 
 }
